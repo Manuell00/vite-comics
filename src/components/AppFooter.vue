67420fd24c9f689dd7fdd9f3c630acd2d0f9f466
Manuell00 <manuell.caselli@gmail.com>
2023-06-02 14:34:02 +0200

    Impostazione progetto

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: "AppFooter",
+}
+</script>
+
+<template></template>
+
+
+<!-- Aggiungo lo scoped perchè lo stile ci interessa solo per il componente relativo -->
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template></template>
+
+
+<!-- Aggiungo lo scoped perchè lo stile ci interessa solo per il componente relativo -->
+<style lang="scss" scoped></style>
