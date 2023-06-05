1cdfb3231ed851696276e1fdd953cfd5e64c5640
Manuell00 <manuell.caselli@gmail.com>
2023-06-05 10:00:34 +0200

    Modifica hover header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,16 +9,36 @@ export default {
     <header>
         <div class="header-image-container"><img src="../../public/img/dc-logo.png" alt=""></div>
         <ul class="header-menu">
-            <li class="header-item"><a href="#">CHARACTERS</a></li>
-            <li class="header-item"><a href="#">COMICS</a></li>
-            <li class="header-item"><a href="#">MOVIES</a></li>
-            <li class="header-item"><a href="#">TV</a></li>
-            <li class="header-item"><a href="#">GAMES</a></li>
-            <li class="header-item"><a href="#">COLLECTIBLES</a></li>
-            <li class="header-item"><a href="#">VIDEOS</a></li>
-            <li class="header-item"><a href="#">FANS</a></li>
-            <li class="header-item"><a href="#">NEWS</a></li>
-            <li class="header-item"><a href="#">SHOP</a></li>
+            <li class="header-item"><a href="#">CHARACTERS</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">COMICS</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">MOVIES</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">TV</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">GAMES</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">COLLECTIBLES</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">VIDEOS</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">FANS</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">NEWS</a>
+                <div class="blue"></div>
+            </li>
+            <li class="header-item"><a href="#">SHOP</a>
+                <div class="blue"></div>
+            </li>
         </ul>
     </header>
 </template>
@@ -57,20 +77,33 @@ header {
         flex-direction: row;
         gap: 25px;
         list-style-type: none;
+        height: 100%;
 
 
         .header-item {
+            height: 100%;
+            display: flex;
+            align-items: center;
+            position: relative;
+
+            .blue {
+                background-color: $primary;
+                position: absolute;
+                height: 8px;
+                width: 100%;
+                bottom: 0;
+                opacity: 0;
+                transition: opacity 0.5s ease-in-out;
+            }
 
             a {
                 font-weight: 700;
                 color: black;
             }
 
-            // Da modificare
-            &:hover {
-                transition: 0.5s;
-                padding-bottom: calc(20px);
-                border-bottom: 4px solid $primary;
+            &:hover .blue {
+                opacity: 1;
+
             }
         }
     }
